5bac7b124208d176adcd1f9c1c08f25ac5c60f59
Seon-U <seonu.kim.kr@gmail.com>
2026-03-30 15:56:23 +0900

    add naver checkstyle

--- BE/src/main/java/com/mgk/bemgk/BeMgkApplication.java
+++ BE/src/main/java/com/mgk/bemgk/BeMgkApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeMgkApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BeMgkApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BeMgkApplication.class, args);
+	}
 }
