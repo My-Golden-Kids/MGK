6cc12d3d52ed52925f1fe83f27e1389fafd8c5e9
Yujin0827 <yjjeon980827@gmail.com>
2026-03-31 16:26:47 +0900

    style: 반응형 웹 구조 설계

--- FE/app/layout.tsx
+++ FE/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const pretendard = localFont({
+  src: './fonts/pretendard/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard',
 });
 
 export const metadata: Metadata = {
@@ -23,11 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ko" className={pretendard.variable}>
+      <body className="bg-[#F2F2F2]">
+          {/* 앱 프레임: 화면을 넘지 않게 고정 */}
+          <div className="mx-auto flex min-h-dvh w-full max-w-[420px] flex-col bg-white md:max-w-[500px] lg:max-w-[640px]">
+            {children}
+          </div>
+      </body>
     </html>
   );
 }
