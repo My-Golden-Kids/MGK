6cbf324930cae503f3b31dbb9c906046f95a13bb
Horok <th2gr22n@gmail.com>
2026-04-07 17:44:34 +0900

    feat: 산책 화면 구현 완성

--- FE/app/health/page.tsx
+++ FE/app/health/page.tsx
@@ -1,14 +1,20 @@
+'use client';
+
 import { BottomNavigation } from '@/components/common/BottomNavigation';
 import { HealthButton } from '@/components/health/HealthButton';
 import { Footprints, NotebookPen, Syringe } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export default function HealthPage() {
+  const router = useRouter();
+
   return (
     <div className="flex min-h-dvh flex-col bg-white">
       <main className="flex flex-1 items-center justify-center px-6 py-6">
         <section className="grid w-full max-w-[596px] grid-cols-2 gap-[20px]">
           <HealthButton
             className="col-span-2"
+            onClick={() => router.push('/health/walk')}
             variant="green"
             size="wide"
             icon={<Footprints />}
--- FE/components/health/walk/MoneyBadge.tsx
+++ FE/components/health/walk/MoneyBadge.tsx
@@ -9,16 +9,16 @@ export default function MoneyBadge({
 }: MoneyBadgeProps) {
   return (
     <div
-      className={`inline-flex items-center gap-2 rounded-full bg-[#F9E8B5] px-2.5 py-1.5 md:px-3.5 md:py-2 lg:px-4.5 lg:py-2.5 ${className}
+      className={`inline-flex items-center gap-2 rounded-full bg-[#F9E8B5] px-2.5 py-1.5 md:px-3.5 md:py-2 lg:px-4.5 lg:py-2 ${className}
       `}
     >
       <img
         src="/images/health/icon_hana_money.png"
         alt="하나머니 아이콘"
-        className="h-3 w-3 md:h-4 md:w-4 lg:h-5 lg:w-5"
+        className="h-5 w-5 sm:h-5 sm:w-5 md:h-6.5 md:w-6.5 lg:h-8 lg:w-8"
       />
 
-      <span className="font-bold text-[14px] text-black leading-none md:text-[18px] lg:text-[22px]">
+      <span className="font-bold text-[20px] text-black leading-none sm:text-[20px] md:text-[28px] lg:text-[34px]">
         {amount.toLocaleString()}
       </span>
     </div>
