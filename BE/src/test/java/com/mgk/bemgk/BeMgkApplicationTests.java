7e524273d2d257caf4a29a8b3336a000baf64c85
Seon-U <seonu.kim.kr@gmail.com>
2026-04-09 10:51:15 +0900

    fix: test에 목빈으로 비젼연결하도록 함

--- BE/src/test/java/com/mgk/bemgk/BeMgkApplicationTests.java
+++ BE/src/test/java/com/mgk/bemgk/BeMgkApplicationTests.java
@@ -2,9 +2,14 @@ package com.mgk.bemgk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
 
 @SpringBootTest
 class BeMgkApplicationTests {
+	@MockitoBean
+	ImageAnnotatorClient imageAnnotatorClient;
 
     @Test
     void contextLoads() {
