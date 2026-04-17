bba87e4c63cb4c7cd13ea73d298599e223143c50
Seon-U <seonu.kim.kr@gmail.com>
2026-04-17 18:21:46 +0900

    fix: signup경로는 api/로 변경
    nextAuth 충돌피하기

--- FE/lib/client-fetch.ts
+++ FE/lib/client-fetch.ts
@@ -57,7 +57,7 @@ export async function signup({
   password,
   accountNum,
 }: SignupParams): Promise<SignupResult> {
-  const res = await fetch('/api/auth/signup', {
+  const res = await fetch('/api/signup', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password, accountNum }),
